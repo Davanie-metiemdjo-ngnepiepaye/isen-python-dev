19aef9e5bd9bbf7ad6dade6d32e5bbf17f553432
Davanie-metiemdjo-ngnepiepaye <ngnepiepayemetiemdjodavanie@gmail.com>
2025-04-26 11:12:02 +0200

    Ajout

--- Project/urls.py
+++ Project/urls.py
@@ -1,29 +1,24 @@
 """django-test URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+Le fichier `urls.py` définit les routes principales de l'application.
+Il redirige chaque préfixe d'URL vers le fichier `urls.py` de chaque application.
+
+Documentation : https://docs.djangoproject.com/en/3.2/topics/http/urls/
 """
+
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
-from django.views.static import serve 
-
+from django.views.static import serve
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-    path('favourite/', include('favourites.urls')),
-    path('user/', include('users.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('admin/', admin.site.urls),                          # Interface admin Django
+    path('', include('home.urls')),                           # App accueil
+    path('favourite/', include('favourites.urls')),           # App favoris
+    path('user/', include('users.urls')),                     # App utilisateurs
+    path('cart/', include('cart.urls')),                      # ✅ App panier (ajouté)
+    
+    # Routes pour accéder aux fichiers médias et statiques pendant le développement
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
